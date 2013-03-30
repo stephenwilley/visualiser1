ed5465b9502b6debdd4ad6f228d59b0215b3732a
Stephen Willey <stephen@esstec.co.uk>
2013-03-30 20:46:45 +0000

    Added minim, loaded an MP3 and started it looping

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ visualiser1
 ===========
 
 Basic audio visualiser made using [http://processing.org](processing.org)
+
 Other libraries used include [http://code.compartmental.net/tools/minim/](minim)
--- src/uk/co/esstec/processing/visualiser1/Gui.java
+++ src/uk/co/esstec/processing/visualiser1/Gui.java
@@ -2,20 +2,30 @@
 package uk.co.esstec.processing.visualiser1;
 
 import processing.core.*;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
 
 public class Gui extends PApplet {
     /**
      * Basic GUI functionality of the applet
      */
     private static final long serialVersionUID = 1L;
-    private static final String version = "1";
+    private static final String version = "2";
     private static final String imagePath = "/Users/stephen/Desktop/visualiser1/"
             + Integer.parseInt(version) + "/";
+
+    Minim minim;
+    AudioPlayer player;
 
     @Override
     public void setup() {
         size(1280, 720, P3D);
         frameRate(25);
+
+        // Load minim and create the player
+        minim = new Minim(this);
+        player = minim.loadFile("/Users/stephen/Downloads/lights-remix.mp3", 2048);
+        player.loop();
     }
 
     @Override
